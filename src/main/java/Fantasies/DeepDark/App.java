c1d0798e56ca98a7596a5667062cf0ddd386dfe9
161250174 <857365274@qq.com>
2017-07-26 13:10:44 +0800

    nothing

---
+++ src/main/java/Fantasies/DeepDark/App.java
@@ -0,0 +1,13 @@
+package Fantasies.DeepDark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
